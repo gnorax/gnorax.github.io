acc2c5363ddf348e61ef092405a818aa53b40e48
gnorax <gnorax@googlemail.com>
2014-08-15 20:14:11 +0200

    pictures and javascript

---
+++ project0/js/script1.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var f = 0;
+function log(){
+	console.log(f);
+	f+=1;
+}
+function print(){
+	alert(f);
+}
